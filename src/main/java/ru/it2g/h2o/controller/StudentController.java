1af5e8a44f4a9be5d0355371757ad633845f77c9
HUAWEI <mr_kiskers@mail.ru>
2024-11-09 23:18:34 +0300

    Для Кирилла

--- src/main/java/ru/it2g/h2o/controller/StudentController.java
+++ src/main/java/ru/it2g/h2o/controller/StudentController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class StudentController {
+public class StudentController { //
 
     private final StudentService studentService;
 
